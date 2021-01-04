1420edc281ab9a28d53c76259777f7c253d77794
Asumreen <ammarsumreen17@gmail.com>
2021-01-04 13:38:17 +0200

    add temp file

---
+++ temp/tempNoUpload.php
@@ -0,0 +1,8 @@
+<?php
+/* This file will not be uploaded to prodection 
+ Dear team please use this file to:
+        1) test any functions out side the site
+        2) add function temporarily 
+        3) add a code for other team member to work on it before add it to the site
+ */
+?>
